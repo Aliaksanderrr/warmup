724facbd6064936e2336ef608235f9c5b2f41fdb
Aliaksanderrr <Midas1989@mail.ru>
2018-09-15 15:32:47 +0300

    my fifteen try

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function warmup(temperature) {
   // your implementation here
+var tempfahrenheit = (9/5)*temperature + 32;
+//tempfahrenheit=Math.round(tempfahrenheit);
+return tempfahrenheit;
 };
